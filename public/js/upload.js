8eacf9d631c6628a1afc641aec3b9f836b3c7beb
van thanh <=>
2020-10-23 00:05:37 +0700

    form-company

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,53 +1,54 @@
-$(document).ready(function(){
-    
-    $('.upload-btn').on('click', function(){
-        $('#upload-input').click();
-        
-        $('.progress-bar').text('0%');
-        $('.progress-bar').width('0%');
-    });
-    
-    $('#upload-input').on('change', function(){
-        var uploadInput = $('#upload-input');
-        
-        if(uploadInput.val() != ''){
-            var formData = new FormData();
-            
-            formData.append('upload', uploadInput[0].files[0]);
-            
-            $.ajax({
-                url: '/upload',
-                type: 'POST',
-                data: formData,
-                processData: false,
-                contentType: false,
-                success: function(data){
-                    uploadInput.val('');
-                },
-                
-                xhr: function(){
-                    var xhr = new XMLHttpRequest();
+$(document).ready(function () {
 
-                    xhr.upload.addEventListener('progress', function(e){
-                        if(e.lengthComputable){
-                            var uploadPercent = e.loaded / e.total;
-                            uploadPercent = (uploadPercent * 100);
+  const $uploadBtn = $('.upload-btn');
+  const $image = $('#image');
+  $uploadBtn.on('click', function () {
+    $image .click();
 
-                            $('.progress-bar').text(uploadPercent+'%');
-                            $('.progress-bar').width(uploadPercent+'%');
+    $('.progress-bar').text('0%');
+    $('.progress-bar').width('0%');
+  });
 
-                            if(uploadPercent === 100){
-                                $('.progress-bar').text('Done');
-                                $('#completed').text('File Uploaded');
-                            }
-                        }
-                    }, false);
+  $('.upload-input').on('change', function () {
+    if ($image.val()) {
+      let formData = new FormData();
 
-                    return xhr;
-                }
-            })
+      formData.append('upload', $image[0].files[0]);
+      console.log("formData", formData)
+
+      $.ajax({
+        url: '/upload',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function () {
+          // $image.val('');
+        },
+
+        xhr: function () {
+          var xhr = new XMLHttpRequest();
+
+          xhr.upload.addEventListener('progress', function (e) {
+            if (e.lengthComputable) {
+              var uploadPercent = e.loaded / e.total;
+              uploadPercent = (uploadPercent * 100);
+
+              $('.progress-bar').text(uploadPercent + '%');
+              $('.progress-bar').width(uploadPercent + '%');
+
+              if (uploadPercent === 100) {
+                $('.progress-bar').text('Done');
+                $('#completed').text('File Uploaded');
+              }
+            }
+          }, false);
+
+          return xhr;
         }
-    })
+      })
+    }
+  })
 })
 
 
--- public/js/validate.js
+++ public/js/validate.js
@@ -1,127 +1,58 @@
-$(document).ready(function(){
-    $('#register').on('click', function(){
-        var name = $.trim($('#name').val());
-        var address = $.trim($('#address').val());
-        var city = $.trim($('#city').val());
-        var country = $.trim($('#country').val());
-        var sector = $.trim($('#sector').val());
-        var website = $.trim($('#website').val());
-        var img = $.trim($('#upload-input').val());
-        
-        var isValid = true;
-        
-        if(name == ''){
-            isValid = false;
-            $('#errorMsg1').html('<div class="alert alert-danger">Name field is empty</div>');
-        }else{
-            $('#errorMsg1').html('');
-        }
-        
-        if(address == ''){
-            isValid = false;
-            $('#errorMsg2').html('<div class="alert alert-danger">Address field is empty</div>');
-        }else{
-            $('#errorMsg2').html('');
-        }
-        
-        if(city == ''){
-            isValid = false;
-            $('#errorMsg3').html('<div class="alert alert-danger">City field is empty</div>');
-        }else{
-            $('#errorMsg3').html('');
-        }
-        
-        if(country == ''){
-            isValid = false;
-            $('#errorMsg4').html('<div class="alert alert-danger">Country field is empty</div>');
-        }else{
-            $('#errorMsg4').html('');
-        }
-        
-        if(sector == ''){
-            isValid = false;
-            $('#errorMsg5').html('<div class="alert alert-danger">Sector field is empty</div>');
-        }else{
-            $('#errorMsg5').html('');
-        }
-        
-        if(website == ''){
-            isValid = false;
-            $('#errorMsg6').html('<div class="alert alert-danger">Website field is empty</div>');
-        }else{
-            $('#errorMsg6').html('');
-        }
-        
-        if(isValid == true){
-            
-            var companyData = {
-                name: name,
-                address: address,
-                city: city,
-                country: country,
-                sector: sector,
-                website: website,
-                img: img
-            };
-            
-            $.ajax({
-                url: '/company/create',
-                type: 'POST',
-                data: companyData,
-                success: function(data){
-                    $('#name').val('');
-                    $('#address').val('');
-                    $('#city').val('');
-                    $('#country').val('');
-                    $('#sector').val('');
-                    $('#website').val('');
-                }
-            });
-            
-        }else{
-            return false;
-        }
-        
+$(document).ready(function () {
+  
+  let $form = $('#companyForm');
+  let $errorsMessage = $('#errorsMessage');
+
+  $form.on('submit', function (e) {
+    e.preventDefault();
+    let fields = {
+      name: '',
+      address: '',
+      city: '',
+      country: '',
+      sector: '',
+      website: '',
+      image: ''
+    };
+    let values = {};
+    let errors = [];
+    let isValid = true;
+
+    Object.keys(fields).forEach(key => {
+      values[key] = $.trim($(`#${key}`).val());
+      if (!values[key]) {
+        isValid = false;
+        errors.push(key);
+      }
     });
-})
+
+    if (errors) {
+      const errorsContent = `
+        <div class="alert alert-danger">
+          <a href="#" class="close" data-dismiss="alert" aria-label="close">
+            &times;
+          </a>
+          ${errors.map(field => 
+            `<div class="mb-2 mt-2"><span class="text-capitalize">${field}</span> field is empty</div>`).join('')
+          }
+        </div>
+      `;
+      $errorsMessage.html(errorsContent);
+    } else $errorsMessage.html('');
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (isValid) {
+      $.ajax({
+        url: '/company/create',
+        type: 'POST',
+        data: values,
+        success: function () {
+          Object.keys(fields).forEach(key => {
+            $(`#${key}`).val('');
+          });
+          $errorsMessage.html('');
+        },
+      });
+    } else return false;
+  });
+});

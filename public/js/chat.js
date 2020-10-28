e95728bed4b3fdd3c2835f8b75a72d7ea65a53be
van thanh <=>
2020-10-28 22:16:03 +0700

    fix eslint

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,20 +1,24 @@
-$(document).ready(function(){
-    var id = $('#receiverId').val();
-    
-    $('#message').click(function(){
-        var message = $.trim($('#msg').val());
-        
-        if(message != ''){
-            $.post('/message/'+id, {
-                message: message,
-                id: id
-            }, function(data){
-                $('#msg').val('');
-            });
-        }
-    });
-    
-    setInterval(function(){
-        $('.msg').load(location.href + ' .msg');
-    }, 200);
+$(document).ready(function () {
+  var id = $('#receiverId').val();
+
+  $('#message').click(function () {
+    var message = $.trim($('#msg').val());
+
+    if (message != '') {
+      $.post(
+        '/message/' + id,
+        {
+          message: message,
+          id: id,
+        },
+        function (data) {
+          $('#msg').val('');
+        },
+      );
+    }
+  });
+
+  setInterval(function () {
+    $('.msg').load(location.href + ' .msg');
+  }, 200);
 });
--- routes/company.js
+++ routes/company.js
@@ -1,6 +1,6 @@
 var formidable = require('formidable');
 var path = require('path');
-var multer  =   require('multer');
+var multer = require('multer');
 var fs = require('fs');
 var async = require('async');
 const upload = require('../config/uploadMiddleware');
@@ -11,16 +11,18 @@ var User = require('../models/user');
 
 var { arrayAverage } = require('../myFunctions');
 
-
 module.exports = (app) => {
-
   app.get('/company/create', (req, res) => {
     var success = req.flash('success');
-    res.render('company/company', { title: 'Company Registration', user: req.user, success: success, noErrors: success.length > 0 });
+    res.render('company/company', {
+      title: 'Company Registration',
+      user: req.user,
+      success: success,
+      noErrors: success.length > 0,
+    });
   });
 
   app.post('/company/create', (req, res) => {
-
     const newCompany = new Company();
     newCompany.name = req.body.name;
     newCompany.address = req.body.address;
@@ -39,10 +41,10 @@ module.exports = (app) => {
 
       req.flash('success', 'Company data has been added.');
       res.redirect('/company/create');
-    })
+    });
   });
 
-  app.post('/upload', upload.single('image') , async (req, res) => {
+  app.post('/upload', upload.single('image'), async (req, res) => {
     // folder upload
     const imagePath = path.join(__dirname, '../public/uploads');
 
@@ -50,7 +52,7 @@ module.exports = (app) => {
     const fileUpload = new Resize(imagePath);
 
     if (!req.file) {
-      res.status(401).json({ error: 'Please provide an image'});
+      res.status(401).json({ error: 'Please provide an image' });
     }
     const filename = await fileUpload.save(req.file.buffer);
     return res.status(200).json({ name: filename });
@@ -58,12 +60,16 @@ module.exports = (app) => {
 
   app.get('/companies', (req, res) => {
     Company.find({}, (err, result) => {
-      res.render('company/companies', { title: 'All Companies || RateMe', user: req.user, data: result });
+      res.render('company/companies', {
+        title: 'All Companies || RateMe',
+        user: req.user,
+        data: result,
+      });
     });
   });
 
   app.get('/company-profile/:id', (req, res) => {
-    Company.findOne({ '_id': req.params.id }, (err, data) => {
+    Company.findOne({ _id: req.params.id }, (err, data) => {
       var avg = arrayAverage(data.ratingNumber);
       console.log(avg);
       res.render('company/company-profile', {
@@ -71,44 +77,57 @@ module.exports = (app) => {
         user: req.user,
         id: req.params.id,
         data: data,
-        average: avg
+        average: avg,
       });
     });
   });
 
   app.get('/company/register-employee/:id', (req, res) => {
-    Company.findOne({ '_id': req.params.id }, (err, data) => {
-      res.render('company/register-employee', { title: 'Register Employee', user: req.user, data: data });
+    Company.findOne({ _id: req.params.id }, (err, data) => {
+      res.render('company/register-employee', {
+        title: 'Register Employee',
+        user: req.user,
+        data: data,
+      });
     });
   });
 
   app.post('/company/register-employee/:id', (req, res, next) => {
     async.parallel([
       function (callback) {
-        Company.update({
-          '_id': req.params.id,
-          'employees.employeeId': { $ne: req.user._id }
-        },
+        Company.update(
           {
-            $push: { employees: { employeeId: req.user._id, employeeFullname: req.user.fullname, employeeRole: req.body.role } }
-          }, (err, count) => {
+            _id: req.params.id,
+            'employees.employeeId': { $ne: req.user._id },
+          },
+          {
+            $push: {
+              employees: {
+                employeeId: req.user._id,
+                employeeFullname: req.user.fullname,
+                employeeRole: req.body.role,
+              },
+            },
+          },
+          (err, count) => {
             if (err) {
               return next(err);
             }
             callback(err, count);
-          });
+          },
+        );
       },
 
       function (callback) {
         async.waterfall([
           function (callback) {
-            Company.findOne({ '_id': req.params.id }, (err, data) => {
+            Company.findOne({ _id: req.params.id }, (err, data) => {
               callback(err, data);
-            })
+            });
           },
 
           function (data, callback) {
-            User.findOne({ '_id': req.user._id }, (err, result) => {
+            User.findOne({ _id: req.user._id }, (err, result) => {
               result.role = req.body.role;
               result.company.name = data.name;
               result.company.image = data.image;
@@ -116,19 +135,19 @@ module.exports = (app) => {
               result.save((err) => {
                 res.redirect('/home');
               });
-            })
-          }
+            });
+          },
         ]);
-      }
+      },
     ]);
   });
 
   app.get('/:name/employees', (req, res) => {
-    Company.findOne({ 'name': req.params.name }, (err, data) => {
+    Company.findOne({ name: req.params.name }, (err, data) => {
       res.render('company/employees', {
         title: 'Company EMployees',
         user: req.user,
-        data: data
+        data: data,
       });
     });
   });
@@ -138,9 +157,9 @@ module.exports = (app) => {
       res.render('company/leaderboard', {
         title: 'Companies Leadebaord || RateMe',
         user: req.user,
-        data: result
+        data: result,
       });
-    }).sort({ 'ratingSum': -1 });
+    }).sort({ ratingSum: -1 });
   });
 
   app.get('/company/search', (req, res) => {
@@ -151,7 +170,7 @@ module.exports = (app) => {
     var name = req.body.search;
     var regex = new RegExp(name, 'i');
 
-    Company.find({ '$or': [{ 'name': regex }] }, (err, data) => {
+    Company.find({ $or: [{ name: regex }] }, (err, data) => {
       if (err) {
         console.log(err);
       }
@@ -160,22 +179,4 @@ module.exports = (app) => {
       res.redirect('/company-profile/' + data[0]._id);
     });
   });
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
